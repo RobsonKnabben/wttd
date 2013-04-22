e4796f7f12f6b900a4728f09bcab06bb81815da0
Robson de Matos Knabben <robson.knabben@gmail.com>
2013-04-22 00:54:06 -0300

    correções

--- eventex/urls.py
+++ eventex/urls.py
@@ -19,6 +19,5 @@ urlpatterns = patterns('',
 )
 
 urlpatterns += patterns('django.views.static',
-    url(r'^static/(?P<path>.*)$', 'serve',
-        {'document_root': settings.STATIC_ROOT}),
+    url(r'^static/(?P<path>.*)$', 'serve', {'document_root': settings.STATIC_ROOT}),
 )
